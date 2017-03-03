336db85674f3b78abf662a37b9c1b96d12e67632
Patrick-Hampton-Avalara <patrick.hampton@avalara.com>
2017-03-03 15:18:44 -0700

    Auto-increment version as part of finishing a release branch

--- GitflowHooks/postFlowReleaseFinish.py
+++ GitflowHooks/postFlowReleaseFinish.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import subprocess
 
 def main():
 	repoPath = sys.argv[1]
@@ -16,6 +17,10 @@ def main():
 
 	with open(versionFile, 'w') as buildVersionFile:
 		buildVersionFile.write(nextVersion)
+		
+	print ('calling commitBuildVersionChanges')
+	commitBuildVersionChanges(repoPath, 'Auto-increment version as part of finishing a release branch');
+	
 	return	
 	
 def incrementVersionPostRelease(currentVersion):
@@ -25,6 +30,36 @@ def incrementVersionPostRelease(currentVersion):
 	versionParts[3] = 0
 	nextReleaseVersion = '{0}.{1}.{2}.{3}\n'.format(versionParts[0], versionParts[1], versionParts[2], versionParts[3])
 	return nextReleaseVersion
-
+	
+def commitBuildVersionChanges(repoPath, commitMessage):
+	
+	print ('Stage')
+	cmd = ['git', 'stage', '*']
+	p = subprocess.Popen(cmd, cwd=repoPath)
+	result = p.wait()
+	
+	print ('result: {0}').format(result)
+	if result != 0:
+		raise Exception('stage call failed')
+	
+	print ('Commit')
+	cmd = ['git', 'commit', '-m', commitMessage]
+	p = subprocess.Popen(cmd, cwd=repoPath)
+	result = p.wait()
+	
+	print ('result: {0}').format(result)
+	if result != 0:
+		raise Exception('commit failed')
+	
+	print ('Push')
+	cmd = ['git', 'push']
+	p = subprocess.Popen(cmd, cwd=repoPath)
+	result = p.wait()
+	
+	print ('result: {0}').format(result)
+	if result != 0:
+		raise Exception('push failed')
+	
+	
 if __name__ == "__main__":
 	main()
